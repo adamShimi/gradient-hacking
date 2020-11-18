6ef7b3d9bfff641a642cc783fea9a33ad601b3e1
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-11-18 20:30:58 +0100

    Clean the code + Tensorboard

--- gradient-hacking-nn.py
+++ gradient-hacking-nn.py
@@ -4,54 +4,61 @@ from tensorflow import keras
 
 # Helper libraries
 import numpy as np
+from datetime import datetime
+import os
 
 # Define and preprocess the data
 
 # The function to approximate is given completely by the training examples
 # (as boolean classification problem). Will probably change in the future.
-training_examples = np.array([[0,0,0],
-                              [0,0,1],
-                              [0,1,0],
-                              [0,1,1],
-                              [1,0,0],
-                              [1,0,1],
-                              [1,1,0],
-                              [1,1,1]])
-training_labels = np.array([[0,1],
-                            [1,0],
-                            [1,0],
-                            [0,1],
-                            [1,0],
-                            [0,1],
-                            [0,1],
-                            [0,1]])
+training_examples = np.array([[0,1,1]])
+training_labels = np.array([[1,0,1]])
 dataset = tf.data.Dataset.from_tensor_slices((tf.constant(training_examples),
                                               tf.constant(training_labels)))
 
-test_examples = tf.constant([[0,0,1],[0,1,1],[1,1,0]])
-test_lbls = tf.constant([[1,0],[0,1],[0,1]])
+test_examples = tf.constant([[0,1,1]])
+test_lbls = tf.constant([[1,0,1]])
+
+
+hidden_nb = 10
 
 # Define the model architecture
 model = keras.Sequential([
   keras.Input(shape=(3,)),
-  keras.layers.Dense(15,activation='sigmoid'),
+  keras.layers.Dense(hidden_nb,activation='sigmoid'),
   keras.layers.Dense(3,activation='sigmoid'),
-  keras.layers.Dense(2,activation='sigmoid'),
 ])
 
 # Define the optimization parameters
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-loss_fn = tf.keras.losses.BinaryCrossentropy()
-metric = tf.keras.metrics.BinaryAccuracy()
+loss_fn = tf.keras.losses.KLDivergence()
+metric = tf.keras.metrics.KLDivergence()
 
-epochs = 100
-batch_size = 8
+epochs = 1000
+batch_size = 1
+
+# Define multiplicative factor for regularization term (the diff of controlled
+# gradient with 0)
+alpha = 10
+
 # This weight is actually the bias of the first neuron of the hidden layer
 # with 15 neurons. I used the bias because it's gradient is approximated by
-# the error, and thus making this gradient 0 controls gradients of the whole
+# the error, and thus making this gradient control gradients of the whole
 # neuron.
 weight_controlled = (1,0)
 target_controlled = 0
+
+
+# Create the right Tensorboard variables
+run_name = "Alpha" + str(alpha) + "Weight" + str(weight_controlled[0]) + str(weight_controlled[1]) + "Target" + str(target_controlled)
+logdir = "logs/scalars/" + run_name + datetime.now().strftime("%Y%m%d-%H%M%S")
+file_writer = tf.summary.create_file_writer(logdir + "/metrics")
+file_writer.set_as_default()
+
+# Create the model folder
+path_models = os.getcwd() + '/models/' + run_name
+os.mkdir(path_models)
+saved = False
 
 # Train for one step on the loss function, plus the difference between
 # the gradient of pos_weight and target_gradr
@@ -62,50 +69,24 @@ def training_step(batch,labels,target_weight,target_grad):
           output = model(batch)  # Compute input reconstruction.
           # Compute loss.
           loss = loss_fn(labels, output)
-          metric.reset_states()
-          metric.update_state(labels,output)
       # Compute the regular gradient.
       grads = tape1.gradient(loss, model.trainable_weights)
       dist_grad = abs(grads[target_weight[0]][target_weight[1]]-target_grad)
     # Compute the gradient of the distance between target_grad and the gradient
     # of target_weight.
-    grads2 = tape2.gradient(dist_grad, model.trainable_weights)
+    grads2 = alpha*tape2.gradient(dist_grad, model.trainable_weights)
     # Update the weights.
     optimizer.apply_gradients(zip(grads, model.trainable_weights))
     optimizer.apply_gradients(zip(grads2, model.trainable_weights))
-    return (loss,metric.result())
+    return (loss,dist_grad)
 
 # Train the model.
-losses = []  # Keep track of the losses over time.
 for step, (batch, labels) in enumerate(dataset.repeat(epochs).batch(batch_size)):
-    (loss,accuracy) = training_step(batch,labels,weight_controlled,target_controlled)
-    # Logging.
-    losses.append(float(loss))
-    print("Step:", step, "Loss:", sum(losses) / len(losses), "Accuracy:", accuracy.numpy())
-
-#Check the gradient of the trained model
-for step, (x, y) in enumerate(dataset.batch(1)):
-  with tf.GradientTape() as tape1:
-      output = model(x)  # Compute input reconstruction.
-      # Compute loss.
-      loss = loss_fn(y, output)
-  # Compute the regular gradient.
-  grads = tape1.gradient(loss, model.trainable_weights)
-  print("Grad for example", x, "is ", grads[1].numpy()[0])
-
-# Compute different metrics on test examples
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(),
-              metrics=['binary_accuracy'])
-
-test_loss, test_acc = model.evaluate(test_examples, test_lbls, verbose = 2)
-
-print('\nTest accuracy:', test_acc)
-
-prob_model = keras.Sequential([
-  model,
-  tf.keras.layers.Softmax()])
-
-predictions = prob_model.predict(test_examples)
-
-print(predictions)
+    loss, dist_grad = training_step(batch,labels,weight_controlled,target_controlled)
+    # Log in Tensorboard
+    tf.summary.scalar('Loss', data = loss, step=step)
+    tf.summary.scalar('Distance of Controlled Grad to 0', data =dist_grad, step=step)
+    # Save model if dist is < 1e-5
+    if not saved and dist_grad < 1e-5:
+      model.save(path_models + "/modelStep" + str(step))
+      saved = True
