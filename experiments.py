82f74848c5eeecdba62f26389f3ba826195a9a3d
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-11-19 16:28:16 +0100

    Make experiments with more variants.

--- experiments.py
+++ experiments.py
@@ -7,15 +7,25 @@ from gradient_hacking import gradientHacker
 
 # Helper libraries
 import numpy as np
+from numpy import random
+
+rng = np.random.default_rng()
 
 # Define and preprocess the data
 
-# The function to approximate is given completely by the training examples
-# (as boolean classification problem). Will probably change in the future.
-training_examples = np.array([[0,1,1]])
-training_labels = np.array([[1,0,1]])
-dataset = tf.data.Dataset.from_tensor_slices((tf.constant(training_examples),
-                                              tf.constant(training_labels)))
+input_size = 3
+output_size = 3
+nb_datasets = 10
+
+# Only one training example, given by numpy RNG
+# training_examples = np.random.rand(nb_datasets,1,input_size)
+training_examples = np.array([[[0,1,0]],[[0,1,0]],[[1,1,0]],[[1,1,0]],[[0,0,0]],[[0,0,0]],[[1,1,1]],[[1,1,1]]])
+# training_labels = np.random.rand(nb_datasets,1,output_size)
+training_labels = np.array([[[1,1,1]],[[1,1,0]],[[1,0,1]],[[1,1,0]],[[0,1,1]],[[1,0,1]],[[0,0,0]],[[0,1,0]]])
+datasets = map(lambda x : tf.data.Dataset \
+                                 .from_tensor_slices((tf.constant(x[0]), \
+                                                     tf.constant(x[1]))), \
+               zip(training_examples,training_labels))
 
 # Define parameters for the gradient hacker
 nb_hidden_layers = 1
@@ -24,18 +34,21 @@ activation_fn = 'sigmoid'
 learning_rate = 1e-3
 epochs = 1000
 batch_size = 1
-regularization_factor = 10
 target_neuron = (1,0)
 target_error = 0
 
-gradientHack = gradientHacker(nb_hidden_layers, \
-                              nb_hidden_per_layer, \
-                              activation_fn, \
-                              learning_rate, \
-                              epochs, \
-                              batch_size, \
-                              regularization_factor, \
-                              target_neuron, \
-                              target_error)
+regularization_factors = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, \
+                          6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]
 
-gradientHack.train(dataset)
+gradientHacks = list(map(lambda x : gradientHacker(nb_hidden_layers, \
+                                                   nb_hidden_per_layer, \
+                                                   activation_fn, \
+                                                   learning_rate, \
+                                                   epochs, \
+                                                   batch_size, \
+                                                   x, \
+                                                   target_neuron, \
+                                                   target_error), \
+                         regularization_factors))
+
+experiments = list(map(lambda x : x.train(datasets), gradientHacks))
