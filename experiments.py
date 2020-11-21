778f02eff7b73805a77028e8339040cadefd87a5
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-11-21 17:49:23 +0100

    Add Reproductibility + GD after threshold.

--- experiments.py
+++ experiments.py
@@ -8,6 +8,16 @@ from gradient_hacking import gradientHacker
 # Helper libraries
 import numpy as np
 from numpy import random
+import random as python_random
+
+# Set up random seed
+random_seeds = [42,349245,2,404523,99850,30452111,646,12]
+chosen_seed = 0
+random_seed = random_seeds[chosen_seed]
+
+np.random.seed(random_seed)
+python_random.seed(random_seed)
+tf.random.set_seed(random_seed)
 
 rng = np.random.default_rng()
 
@@ -17,11 +27,9 @@ input_size = 3
 output_size = 3
 nb_datasets = 10
 
-# Only one training example, given by numpy RNG
-# training_examples = np.random.rand(nb_datasets,1,input_size)
-training_examples = np.array([[[0,1,0]],[[0,1,0]],[[1,1,0]],[[1,1,0]],[[0,0,0]],[[0,0,0]],[[1,1,1]],[[1,1,1]]])
-# training_labels = np.random.rand(nb_datasets,1,output_size)
-training_labels = np.array([[[1,1,1]],[[1,1,0]],[[1,0,1]],[[1,1,0]],[[0,1,1]],[[1,0,1]],[[0,0,0]],[[0,1,0]]])
+# Only one training example
+training_examples = np.array([[[0,1,0]]])
+training_labels = np.array([[[1,1,0]]])
 datasets = list(map(lambda x : tf.data.Dataset \
                                  .from_tensor_slices((tf.constant(x[0]), \
                                                      tf.constant(x[1]))), \
@@ -36,6 +44,7 @@ epochs = 1000
 batch_size = 1
 target_neuron = (1,0)
 target_error = 0
+threshold = 1e-3
 
 regularization_factors = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, \
                           6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]
@@ -48,7 +57,13 @@ gradientHacks = list(map(lambda x : gradientHacker(nb_hidden_layers, \
                                                    batch_size, \
                                                    x, \
                                                    target_neuron, \
-                                                   target_error), \
+                                                   target_error, \
+                                                   threshold,
+                                                   random_seed), \
                          regularization_factors))
 
-list(map(lambda x : x.train(datasets), gradientHacks))
+count = 1
+for gradientHack in gradientHacks:
+  gradientHack.train(datasets)
+  print("Training ", count, " Finished")
+  count+=1
--- gradient_hacking.py
+++ gradient_hacking.py
@@ -4,10 +4,21 @@ from tensorflow import keras
 
 # Helper libraries
 import numpy as np
+from numpy import random
+import random as python_random
 from datetime import datetime
 import os
 
 class gradientHacker:
+
+  init_model = None
+
+  def get_model(self):
+    if self.init_model == None:
+      self.init_model = self.model.get_weights()
+    else:
+      self.model.set_weights(self.init_model)
+
 
   def __init__(self, \
                nb_hidden_layers, \
@@ -18,7 +29,9 @@ class gradientHacker:
                batch_size, \
                regularization_factor, \
                target_neuron, \
-               target_error):
+               target_error, \
+               threshold, \
+               random_seed):
 
     self.nb_hidden_layers = nb_hidden_layers
     self.nb_hidden_per_layer = nb_hidden_per_layer
@@ -28,6 +41,8 @@ class gradientHacker:
     self.model = keras.Sequential([keras.Input(shape=(3,))] \
                                   + hidden_layers \
                                   + [keras.layers.Dense(3,activation=activation_fn)])
+    self.get_model()
+
     self.optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     self.loss_fn = tf.keras.losses.KLDivergence()
     self.epochs = epochs
@@ -38,14 +53,17 @@ class gradientHacker:
     # Define neurons to be controlled and target error for this neuron
     self.target_neuron = target_neuron
     self.target_error = target_error
+    self.threshold = threshold
+    self.random_seed = random_seed
+
+    self.regularize = True
 
     # Create the right Tensorboard variables
     run_name = "Regularization" + str(self.regularization_factor) \
                + "Neuron" + str(self.target_neuron[0]) + ',' + str(self.target_neuron[1]) \
                + "Target" + str(self.target_error) + '-'
     logdir = "logs/scalars/" + run_name + datetime.now().strftime("%Y%m%d-%H%M%S")
-    file_writer = tf.summary.create_file_writer(logdir + "/metrics")
-    file_writer.set_as_default()
+    self.file_writer = tf.summary.create_file_writer(logdir + "/metrics")
 
     # Create the model folder
     # path_models = os.getcwd() + '/models/' + run_name
@@ -66,28 +84,38 @@ class gradientHacker:
       # Compute the distance of error of neuron (the gradient of the bias)
       # with targeted value.
       dist_grad = abs(grads[self.target_neuron[0]][self.target_neuron[1]]-self.target_error)
-    # Compute the gradient of the distance between target_grad and the gradient
-    # of target_weight.
-    grads2 = tape2.gradient(dist_grad, self.model.trainable_weights)
-    grads2 = list(map(lambda x : tf.math.scalar_mul(self.regularization_factor, x),grads))
     # Update the weights.
     self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
-    self.optimizer.apply_gradients(zip(grads2, self.model.trainable_weights))
+    if self.regularize:
+      # Compute the gradient of the distance between target_grad and the gradient
+      # of target_weight.
+      grads2 = tape2.gradient(dist_grad, self.model.trainable_weights)
+      grads2 = list(map(lambda x : tf.math.scalar_mul(self.regularization_factor, x),grads))
+      self.optimizer.apply_gradients(zip(grads2, self.model.trainable_weights))
     return (loss,dist_grad)
 
   # Train the model.
   def train(self,datasets):
     first = True
     for dataset in datasets:
+      # Reseed
+      np.random.seed(self.random_seed)
+      python_random.seed(self.random_seed)
+      tf.random.set_seed(self.random_seed)
       # Reset the model for the new dataset. Here so that reset only happens if there is
       # another dataset.
       if not first:
         self.reset_model()
+      self.regularize = True
       for step, (batch, labels) in enumerate(dataset.repeat(self.epochs).batch(self.batch_size)):
         loss, dist_grad = self.training_step(batch,labels)
         # Log in Tensorboard
-        tf.summary.scalar('Loss', data=loss, step=step)
-        tf.summary.scalar('Distance of Controlled Grad to 0', data=dist_grad, step=step)
+        with self.file_writer.as_default():
+          tf.summary.scalar('Loss', data=loss, step=step)
+          tf.summary.scalar('Distance of Controlled Grad to 0', data=dist_grad, step=step)
+        if self.regularize and dist_grad < self.threshold:
+          self.regularize = False
+          print("Back to normal gradient descent!")
         # Save model if dist is < 1e-5
         # if not saved and dist_grad < 1e-5:
         #   model.save(path_models + "/modelStep" + str(step))
@@ -95,18 +123,12 @@ class gradientHacker:
 
 
   def reset_model(self):
-    hidden_layers = \
-      [keras.layers.Dense(self.nb_hidden_per_layer,activation=self.activation_fn)] \
-      * self.nb_hidden_layers
-    self.model = keras.Sequential([keras.Input(shape=(3,))] \
-                                  + hidden_layers \
-                                  + [keras.layers.Dense(3,activation=self.activation_fn)])
+    self.get_model()
 
     # Create the right Tensorboard variables
     run_name = "Regularization" + str(self.regularization_factor) \
                + "Neuron" + str(self.target_neuron[0]) + ',' + str(self.target_neuron[1]) \
                + "Target" + str(self.target_error) + '-'
     logdir = "logs/scalars/" + run_name + datetime.now().strftime("%Y%m%d-%H%M%S")
-    file_writer = tf.summary.create_file_writer(logdir + "/metrics")
-    file_writer.set_as_default()
+    self.file_writer = tf.summary.create_file_writer(logdir + "/metrics")
 
