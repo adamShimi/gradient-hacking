9afd4698d3d1b3adadfc32277edb6b3d138923b1
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-12-30 15:06:47 +0100

    Look at what it means for smaller hidden layer.

--- bitflip_experiments.py
+++ bitflip_experiments.py
@@ -13,74 +13,83 @@ import numpy as np
 from numpy import random
 import random as python_random
 
-# Create training example (bitflip)
-
-data_size = 3
-training_set_size = 2
-
-training_examples,training_labels = bitflip(data_size,training_set_size)
-
 # Set up random seed
 random_seeds = [42,349245,2,404523,99850,30452111,646,12]
 
 # Define parameters for the gradient hacker
 nb_hidden_layers = 1
-nb_hidden_per_layer = 15
+nb_hidden_per_layer = 3
 activation_fn = 'sigmoid'
 learning_rate = 1e-3
 epochs = 1000
-batch_size = training_set_size
 # Each pair is (coeff regularization, coeff loss),
 # and the elements are [initial coeffs, epoch starting annealing,
 #                       final coeffs, epoch ending annealing]
 annealings = [[(x,1.0),1000,(0.0,0.0),-1] for x in np.arange(0.0,6.0,.5)]
 target_neuron = (1,0)
 target_error = 0
-threshold = 1e-4
+threshold = 1e-2
 
-# Correct that for bigger dataset.
-for dataset_index in [0]:#range(len(datasets)):
-  training_example = np.array(list(next(training_examples)))
-  training_label = np.array(list(next(training_labels)))
+min_datasize = 3
+max_datasize = 8
+
+for data_size in range(min_datasize,max_datasize+1):
+
+  # Create training example (bitflip)
+  training_set_size = 2**data_size
+  patterns = [list(range(0, training_set_size))]
+
+  training_examples,training_labels = bitflip(data_size,patterns)
+
+  dataset_index = 0
+
+  training_example = np.array(training_examples[dataset_index])
+  training_label = np.array(training_labels[dataset_index])
   dataset = tf.data.Dataset \
                    .from_tensor_slices((tf.constant(training_example), \
                                         tf.constant(training_label)))
-  for seed_index in [0]:# range(len(random_seeds)):
-    for annealing in annealings:
 
-      random_seed = random_seeds[seed_index]
+  min_batch = 1
+  max_batch = training_set_size
 
-      np.random.seed(random_seed)
-      python_random.seed(random_seed)
-      tf.random.set_seed(random_seed)
+  for batch_size in [min_batch,max_batch]:#range(min_batch,max_batch+1):
+    for seed_index in [0]:# range(len(random_seeds)):
+      for annealing in annealings:
 
-      folder = 'Bitflip' \
-               + '_Size_' + str(data_size) \
-               + '_Nb_Examples_' + str(training_set_size) \
-               + '_Batch_Size_' + str(batch_size) \
-               + '_Hidden_' + str(nb_hidden_per_layer) \
-               + '_Neuron_' + str(target_neuron[0]) + ',' + str(target_neuron[1]) \
-               + '_Target_' + str(target_error) \
-               + '/'
+        random_seed = random_seeds[seed_index]
 
-      # Just for current experiment
-      name = 'Training_' + str(dataset_index) \
-             + '_Seed_' + str(seed_index) \
-             + '_Annealing_' + str(annealing) \
+        np.random.seed(random_seed)
+        python_random.seed(random_seed)
+        tf.random.set_seed(random_seed)
 
-      gradientHack = gradientHacker(folder + name, \
-                                    data_size, \
-                                    nb_hidden_layers, \
-                                    nb_hidden_per_layer, \
-                                    activation_fn, \
-                                    learning_rate, \
-                                    epochs, \
-                                    batch_size, \
-                                    annealing, \
-                                    target_neuron, \
-                                    target_error, \
-                                    threshold,
-                                    random_seed)
+        folder = 'Bitflip' \
+                 + '_Size_' + str(data_size) \
+                 + '_Nb_Examples_' + str(training_set_size) \
+                 + '_Batch_Size_' + str(batch_size) \
+                 + '_Hidden_' + str(nb_hidden_per_layer) \
+                 + '_Neuron_' + str(target_neuron[0]) + ',' + str(target_neuron[1]) \
+                 + '_Target_' + str(target_error) \
+                 + '_Threshold_' + str(threshold) \
+                 + '/'
 
-      gradientHack.train(dataset)
-      print("Training ", name, " Finished")
+        # Just for current experiment
+        name = 'Training_' + str(dataset_index) \
+               + '_Seed_' + str(seed_index) \
+               + '_Annealing_' + str(annealing) \
+
+        gradientHack = gradientHacker(folder + name, \
+                                      data_size, \
+                                      nb_hidden_layers, \
+                                      nb_hidden_per_layer, \
+                                      activation_fn, \
+                                      learning_rate, \
+                                      epochs, \
+                                      batch_size, \
+                                      annealing, \
+                                      target_neuron, \
+                                      target_error, \
+                                      threshold,
+                                      random_seed)
+
+        gradientHack.train(dataset)
+        print("Training ", name, " Finished")
